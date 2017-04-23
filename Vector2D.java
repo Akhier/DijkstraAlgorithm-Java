6abfbfb780dc9c409bc4199ea8088034f13bf5dd
Akhier <akherthedragonhearted@gmail.com>
2017-04-22 21:32:02 -0400

    Added class description to Vector2D

--- Vector2D.java
+++ Vector2D.java
@@ -1,8 +1,8 @@
 /**
+ * "Vector2D" is a class to represent a point on a 2d map and it's connections to other points
  * @author Akhier Dragonheart
  * @version 1.0
  */
-
 public class Vector2D {
 	public static int INFINITY = -1;
 	public int X, Y, aggregateCost;
