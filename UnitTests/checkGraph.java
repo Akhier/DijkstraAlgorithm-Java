1824bae1f73e5018bdb0cefa126260449771648f
Akhier <akherthedragonhearted@gmail.com>
2017-04-24 13:23:08 -0400

    added 'test' to visually check Graph
    While I could check all of these things with asserts (and do with most
    of them) being able to visually confirm it is nice. Also figured out how
    to get a test suite to use utf8 so the characters show up right.
    
    Also will have to go change testGraphMaker as the provided boolean[][]
    isn't 'right'.

---
+++ UnitTests/checkGraph.java
@@ -0,0 +1,80 @@
+package UnitTests;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import DijkstraAlgorithm.Graph;
+import DijkstraAlgorithm.GraphMaker;
+import DijkstraAlgorithm.Vector2D;
+
+/**
+ * @author Dragonheart
+ */
+public class checkGraph {
+	private boolean[][] map;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		map = new boolean[7][7];
+		map[0][0] = true; map[1][0] = true; map[2][0] = false; map[3][0] = true; map[4][0] = true; map[5][0] = true; map[6][0] = true;
+		map[0][1] = false; map[1][1] = true; map[2][1] = false; map[3][1] = false; map[4][1] = true; map[5][1] = false; map[6][1] = true;
+		map[0][2] = true; map[1][2] = true; map[2][2] = true; map[3][2] = false; map[4][2] = true; map[5][2] = false; map[6][2] = true;
+		map[0][3] = true; map[1][3] = false; map[2][3] = true; map[3][3] = false; map[4][3] = true; map[5][3] = false; map[6][3] = true;
+		map[0][4] = true; map[1][4] = false; map[2][4] = true; map[3][4] = true; map[4][4] = true; map[5][4] = false; map[6][4] = true;
+		map[0][5] = true; map[1][5] = true; map[2][5] = true; map[3][5] = false; map[4][5] = false; map[5][5] = true; map[6][5] = true;
+		map[0][6] = false; map[1][6] = false; map[2][6] = false; map[3][6] = false; map[4][6] = false; map[5][6] = false; map[6][6] = true;
+	}
+
+	@Test
+	public final void test() {
+		Graph graph = GraphMaker.rectangularMap(map, 1, -1);
+		Graph graph2 = GraphMaker.rectangularMap(map, 1, 1);
+		ArrayList<Vector2D> nodes = graph.allNodes();
+		ArrayList<Vector2D> nodes2 = graph2.allNodes();
+		graph.sourceVector(nodes.get(0));
+		graph2.sourceVector(nodes2.get(0));
+		graph.calculateShortestPath();
+		graph2.calculateShortestPath();
+		int[][] test = new int[7][7];
+		int[][] test2 = new int[7][7];
+		for(int y = 0; y < 7; y++) {
+			for(int x = 0; x < 7; x++) {
+				test[x][y] = -1;
+				test2[x][y] = -1;
+			}
+		}
+		for(Vector2D node : nodes) {
+			test[node.X][node.Y] = node.aggregateCost;
+		}
+		for(Vector2D node2 : nodes2) {
+			test2[node2.X][node2.Y] = node2.aggregateCost;
+		}
+		System.out.printf("┌──┬──┬──┬──┬──┬──┬──┐   ┌──┬──┬──┬──┬──┬──┬──┐\n");
+		for(int y = 0; y < 7; y++) {
+			System.out.printf("│");
+			for(int x = 0; x < 7; x++) {
+				if(test[x][y] == -1) {
+					System.out.printf("▓▓│");
+				} else {
+					System.out.printf("%2d│", test[x][y]);
+				}
+			}
+			System.out.printf("   │");
+			for(int x = 0; x < 7; x++) {
+				if(test2[x][y] == -1) {
+					System.out.printf("▓▓│");
+				} else {
+					System.out.printf("%2d│", test2[x][y]);
+				}
+			}
+			if(y < 6) {
+				System.out.printf("\n├──┼──┼──┼──┼──┼──┼──┤   ├──┼──┼──┼──┼──┼──┼──┤\n");
+			} else {
+				System.out.printf("\n└──┴──┴──┴──┴──┴──┴──┘   └──┴──┴──┴──┴──┴──┴──┘\n");
+			}
+		}
+	}
+}
