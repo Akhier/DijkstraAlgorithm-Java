c8b277918d102498d69a80028a4fc9727bde5ec5
Akhier <akherthedragonhearted@gmail.com>
2017-04-30 10:40:34 -0400

    Setup for the test

--- src/test/java/com/dragonheart/test/testEdge.java
+++ src/test/java/com/dragonheart/test/testEdge.java
@@ -3,10 +3,19 @@ package com.dragonheart.test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import com.dragonheart.dijkstra.Point;
+import com.dragonheart.dijkstra.Edge;
 
 public class testEdge {
+	private Point a, b;
+	private Edge A, B;
+
 	@Before
 	public void setUp() throws Exception {
+		a = new Point(0, 0);
+		b = new Point(1, 1);
+		A = new Edge(a, b, 1);
+		B = new Edge(a, b, 2);
 	}
 
 	@Test
