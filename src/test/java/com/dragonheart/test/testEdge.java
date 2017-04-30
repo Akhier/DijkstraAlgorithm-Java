69401a82eb7a23c7bdeffa26ff0e44f35b7af5a2
Akhier <akherthedragonhearted@gmail.com>
2017-04-30 10:36:59 -0400

    generating Edge test

---
+++ src/test/java/com/dragonheart/test/testEdge.java
@@ -0,0 +1,16 @@
+package com.dragonheart.test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testEdge {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+}
