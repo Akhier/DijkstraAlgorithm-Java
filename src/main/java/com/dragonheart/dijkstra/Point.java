d72007852ba1718d75fbde4ff16958be59fde5e0
Akhier <akherthedragonhearted@gmail.com>
2017-04-30 00:47:26 -0400

    Well I forgot to do more commits
    So this is the whole Point class. pointId is final as it doesn't get to
    change after set. Visited gets not only a getter but 2 setters because
    when it becomes visited we need to increment totalvisited. Also this
    time toString also informs you of the totalvisited.

---
+++ src/main/java/com/dragonheart/dijkstra/Point.java
@@ -0,0 +1,51 @@
+package com.dragonheart.dijkstra;
+
+public class Point implements Comparable<Point>{
+	public int X, Y, Z;
+	public Double aggregateCost;
+	final public int pointId;
+	public static int TotalVisited = 0;
+	private static int pointIdCount = -1;
+	private boolean visited;
+	public boolean isVisited() {
+		return visited;
+	}
+	public void setVisited() {
+		visited = true;
+		TotalVisited++;
+	}
+	public void resetVisited() {
+		visited = false;
+	}
+
+	public Point(int x, int y, int z, Double cost) {
+		this.X = x;
+		this.Y = y;
+		this.Z = z;
+		this.aggregateCost = cost;
+		this.pointId = ++pointIdCount;
+		this.visited = false;
+	}
+
+	public Point(int x, int y, int z) {
+		this(x, y, z, null);
+	}
+
+	public Point(int x, int y, Double cost) {
+		this(x, y, 0, cost);
+	}
+
+	public Point(int x, int y) {
+		this(x, y, 0, null);
+	}
+
+	@Override
+	public int compareTo(Point otherpoint) {
+		return aggregateCost.compareTo(otherpoint.aggregateCost);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Point Id: %d, X: %d, Y: %d, Z: %d, Aggregate Cost: %f, Total Visited: %d;", pointId, X, Y, Z, aggregateCost, TotalVisited);
+	}
+}
