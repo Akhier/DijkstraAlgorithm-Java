01c62a05b4fe51e00ed4809820f2942605cedb5d
Akhier <akherthedragonhearted@gmail.com>
2017-04-30 13:45:30 -0400

    test for multiple sourceNodes

--- src/test/java/com/dragonheart/test/testDijkstraGraph.java
+++ src/test/java/com/dragonheart/test/testDijkstraGraph.java
@@ -9,8 +9,8 @@ import com.dragonheart.dijkstra.Edge;
 import com.dragonheart.dijkstra.DijkstraGraph;
 
 public class testDijkstraGraph {
-	private Point a, b, c, d, e;
-	private Edge A, B, C, D, E, F, G;
+	private Point a, b, c, d, e, f;
+	private Edge A, B, C, D, E, F, G, H;
 	private DijkstraGraph graph;
 
 	@Before
@@ -20,6 +20,7 @@ public class testDijkstraGraph {
 		c = new Point(2, 2);
 		d = new Point(5, 2);
 		e = new Point(2, 5);
+		f = new Point(0, 5);
 		A = new Edge(a, b, 1.0);
 		B = new Edge(a, c, 3.0);
 		C = new Edge(a, d, 5.0);
@@ -27,12 +28,14 @@ public class testDijkstraGraph {
 		E = new Edge(b, e, 3.0);
 		F = new Edge(c, e, 1.0);
 		G = new Edge(d, e, 1.0);
+		H = new Edge(b, f, 1.0);
 		graph = new DijkstraGraph();
 		graph.addPoint(a);
 		graph.addPoint(b);
 		graph.addPoint(c);
 		graph.addPoint(d);
 		graph.addPoint(e);
+		graph.addPoint(f);
 		graph.addEdge(A);
 		graph.addEdge(B);
 		graph.addEdge(C);
@@ -40,6 +43,7 @@ public class testDijkstraGraph {
 		graph.addEdge(E);
 		graph.addEdge(F);
 		graph.addEdge(G);
+		graph.addEdge(H);
 	}
 
 	@Test
@@ -62,5 +66,18 @@ public class testDijkstraGraph {
 		assertTrue(path.get(1) == b);
 		assertTrue(path.get(2) == c);
 		assertTrue(path.get(3) == e);
+	}
+
+	@Test
+	public final void testGetPathFrom_WithMultipleSourceNodes() {
+		ArrayList<Point> nodes = new ArrayList<Point>();
+		nodes.add(e);
+		nodes.add(f);
+		graph.setSourceNodes(nodes);
+		graph.processGraph()
+		ArrayList<Point> path = graph.getPathFrom(a);
+		assertTrue(path.get(0) == a);
+		assertTrue(path.get(1) == b);
+		assertTrue(path.get(2) == f);
 	}
 }
