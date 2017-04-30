829d636c9ec69d518690b1be2c56ef1f7134b44a
Akhier <akherthedragonhearted@gmail.com>
2017-04-30 15:54:42 -0400

    Add sources 4 different ways!

--- src/main/java/com/dragonheart/dijkstra/DijkstraGraph.java
+++ src/main/java/com/dragonheart/dijkstra/DijkstraGraph.java
@@ -1,15 +1,16 @@
 package com.dragonheart.dijkstra;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DijkstraGraph {
 	private ArrayList<Edge> listOfEdges;
-	private ArrayList<Point> listOfPoints, sourceNodes;
+	private ArrayList<Point> listOfPoints, sourcePoints;
 
 	public DijkstraGraph() {
 		this.listOfEdges = new ArrayList<Edge>();
 		this.listOfPoints = new ArrayList<Point>();
-		this.sourceNodes = new ArrayList<Point>();
+		this.sourcePoints = new ArrayList<Point>();
 	}
 
 	public void addPoint(Point point) {
@@ -18,5 +19,28 @@ public class DijkstraGraph {
 
 	public void addEdge(Edge edge) {
 		listOfEdges.add(edge);
+	}
+
+	public void addSource(Point point, Double cost) {
+		if(listOfPoints.contains(point)) {
+			point.aggregateCost = cost;
+			sourcePoints.add(point);
+		}
+	}
+
+	public void addSources(List<Point> points, Double cost) {
+		for(Point point : points) {
+			addSource(point, cost);
+		}
+	}
+
+	public void setSource(Point point, Double cost) {
+		sourcePoints = new ArrayList<Point>();
+		addSource(point, cost);
+	}
+
+	public void setSources(List<Point> points, Double cost) {
+		sourcePoints = new ArrayList<Point>();
+		addSources(points, cost);
 	}
 }
