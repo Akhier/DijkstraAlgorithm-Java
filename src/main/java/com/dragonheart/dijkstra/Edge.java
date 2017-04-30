ba13036d54cb729a4cccd508c0bdf7e2f02d75c7
Akhier <akherthedragonhearted@gmail.com>
2017-04-30 11:33:31 -0400

    The points are now public as I can't find a reason not to

--- src/main/java/com/dragonheart/dijkstra/Edge.java
+++ src/main/java/com/dragonheart/dijkstra/Edge.java
@@ -1,9 +1,15 @@
 package com.dragonheart.dijkstra;
 
-public class Edge {
+public class Edge implements Comparable<Edge>{
+	public Point A, B;
 	public Double cost;
-	private Point A, B;
-	
-	public Edge(Point a, Point b, Double i) {
+	public final int edgeId;
+	private static int edgeIdCount = -1;
+
+	public Edge(Point a, Point b, Double cost) {
+		this.A = a;
+		this.B = b;
+		this.cost = cost;
+		this.edgeId = ++edgeIdCount;
 	}
 }
