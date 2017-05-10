019f14d833bc3423aa3d771a67cb7d16ea8c3e47
Akhier <akherthedragonhearted@gmail.com>
2017-05-10 09:45:58 -0400

    removed cost from Edge

--- src/main/java/com/dragonheart/dijkstra/Edge.java
+++ src/main/java/com/dragonheart/dijkstra/Edge.java
@@ -4,14 +4,12 @@ package com.dragonheart.dijkstra;
  * "Edge" is a class made to represent the connection between 2 nodes in a dijkstra map
  * @author Akhier Dragonheart
  */
-public class Edge implements Comparable<Edge>{
+public class Edge {
 	public Point A, B;
-	public Double cost;
 
 	public Edge(Point a, Point b, Double cost) {
 		this.A = a;
 		this.B = b;
-		this.cost = cost;
 	}
 
 	/**
@@ -27,10 +25,5 @@ public class Edge implements Comparable<Edge>{
 		} else {
 			return null;
 		}
-	}
-
-	@Override
-	public int compareTo(Edge otheredge) {
-		return cost.compareTo(otheredge.cost);
 	}
 }
