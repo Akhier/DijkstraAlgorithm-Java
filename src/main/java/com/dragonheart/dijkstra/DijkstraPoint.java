a0bcf6bbf5740ff544bfd393e675a41e3f10abac
Akhier <akherthedragonhearted@gmail.com>
2017-05-10 21:48:45 -0400

    DijkstraPoint interface added

---
+++ src/main/java/com/dragonheart/dijkstra/DijkstraPoint.java
@@ -0,0 +1,9 @@
+package com.dragonheart.dijkstra;
+
+public interface DijkstraPoint {
+	/**
+	 * Returns the cost to enter the point
+	 * @return Double with the entry cost
+	 */
+	public Double costToEnter();
+}
