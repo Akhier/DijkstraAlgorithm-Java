cbf01ceb3c2bd03047bf43f13730d16abafd30a3
Akhier <akherthedragonhearted@gmail.com>
2017-05-10 23:52:25 -0400

    added option to make a graph with points that have cost already

--- src/main/java/com/dragonheart/dijkstra/DijkstraGraphFactory.java
+++ src/main/java/com/dragonheart/dijkstra/DijkstraGraphFactory.java
@@ -2,7 +2,7 @@ package com.dragonheart.dijkstra;
 
 public class DijkstraGraphFactory {
 	/**
-	 * Create a DijkstraGraph from a 2D array of points that implements DijkstraPoint were null in the array means a tile that isn't walkable
+	 * Create a DijkstraGraph from a 2D array of points that implements DijkstraPoint where null in the array means a tile that isn't walkable
 	 * @param pointmap is a 2D array of points that implements DijkstraPoint
 	 * @param costtoenter is a Double representing how much tiles cost to enter
 	 * @param allowdiagonal is a boolean of if diagonal movement is allowed
@@ -31,6 +31,39 @@ public class DijkstraGraphFactory {
 						if(y != 0 && pointmap[x][y - 1] != null) {
 							graph.addConnection(pointmap[x][y], pointmap[x][y - 1]);
 						}
+					}
+				}
+			}
+		}
+		return graph;
+	}
+
+	/**
+	 * Create a DijkstraGraph from a 2D array of points that implements DijkstraPoint with pre-existing entry cost where null means the point isn't walkable
+	 * @param pointmap is a 2D array of points that implements DijkstraPoint and the points have a pre-set entry cost to them
+	 * @param allowdiagonal is a boolean of if diagonal movement is allowed
+	 * @return DijkstraGraph
+	 */
+	public static DijkstraGraph dijkstraGraphFrom2DDijkstraPointArray(DijkstraPoint[][] pointmap, boolean allowdiagonal) {
+		DijkstraGraph graph = new DijkstraGraph();
+		int width = pointmap.length, height = pointmap[0].length;
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				if(pointmap[x][y] != null && pointmap[x][y].costToEnter() > 0 && pointmap[x][y].costToEnter() != null) {
+					graph.addPoint(pointmap[x][y]);
+					if(allowdiagonal) {
+						if(x != 0 && y != 0 && pointmap[x - 1][y - 1] != null) {
+							graph.addConnection(pointmap[x][y], pointmap[x - 1][y - 1]);
+						}
+						if(x != width - 1 && y != 0 && pointmap[x + 1][y - 1] != null) {
+							graph.addConnection(pointmap[x][y], pointmap[x + 1][y - 1]);
+						}
+					}
+					if(x != 0 && pointmap[x - 1][y] != null) {
+						graph.addConnection(pointmap[x][y], pointmap[x - 1][y]);
+					}
+					if(y != 0 && pointmap[x][y - 1] != null) {
+						graph.addConnection(pointmap[x][y], pointmap[x][y - 1]);
 					}
 				}
 			}
