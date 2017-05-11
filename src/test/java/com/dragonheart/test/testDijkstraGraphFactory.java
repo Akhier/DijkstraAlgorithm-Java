c56dcf1f7bd91e5a183c86ad2f565def36d505e1
Akhier <akherthedragonhearted@gmail.com>
2017-05-10 23:53:07 -0400

    rename factory test

--- src/test/java/com/dragonheart/test/testDijkstraGraphFactory.java
+++ src/test/java/com/dragonheart/test/testDijkstraGraphFactory.java
@@ -11,9 +11,12 @@ import com.dragonheart.dijkstra.DijkstraGraph;
 import com.dragonheart.dijkstra.DijkstraGraphFactory;
 import com.dragonheart.dijkstra.DijkstraPoint;
 
-public class testDijkstraGraphFrom2DDijkstraPointArray {
+public class testDijkstraGraphFactory {
 	private class Point implements DijkstraPoint {
 		public Double entryCost;
+		public Point(Double cost) {
+			this.entryCost = cost;
+		}
 		@Override
 		public Double costToEnter() {
 			return entryCost;
@@ -29,13 +32,13 @@ public class testDijkstraGraphFrom2DDijkstraPointArray {
 	public void setUp() throws Exception {
 		int width = 7, height = 7;
 		pointMap = new Point[width][height];
-		pointMap[0][0] = new Point(); pointMap[1][0] = new Point(); pointMap[2][0] = null; pointMap[3][0] = new Point(); pointMap[4][0] = new Point(); pointMap[5][0] = new Point(); pointMap[6][0] = new Point();
-		pointMap[0][1] = null; pointMap[1][1] = new Point(); pointMap[2][1] = null; pointMap[3][1] = null; pointMap[4][1] = new Point(); pointMap[5][1] = null; pointMap[6][1] = new Point();
-		pointMap[0][2] = new Point(); pointMap[1][2] = new Point(); pointMap[2][2] = new Point(); pointMap[3][2] = null; pointMap[4][2] = new Point(); pointMap[5][2] = null; pointMap[6][2] = new Point();
-		pointMap[0][3] = new Point(); pointMap[1][3] = null; pointMap[2][3] = new Point(); pointMap[3][3] = null; pointMap[4][3] = new Point(); pointMap[5][3] = null; pointMap[6][3] = new Point();
-		pointMap[0][4] = new Point(); pointMap[1][4] = null; pointMap[2][4] = new Point(); pointMap[3][4] = new Point(); pointMap[4][4] = new Point(); pointMap[5][4] = null; pointMap[6][4] = new Point();
-		pointMap[0][5] = new Point(); pointMap[1][5] = new Point(); pointMap[2][5] = new Point(); pointMap[3][5] = null; pointMap[4][5] = null; pointMap[5][5] = new Point(); pointMap[6][5] = new Point();
-		pointMap[0][6] = null; pointMap[1][6] = null; pointMap[2][6] = null; pointMap[3][6] = null; pointMap[4][6] = null; pointMap[5][6] = null; pointMap[6][6] = new Point();
+		pointMap[0][0] = new Point(1.0); pointMap[1][0] = new Point(1.0); pointMap[2][0] = null; pointMap[3][0] = new Point(1.0); pointMap[4][0] = new Point(1.0); pointMap[5][0] = new Point(1.0); pointMap[6][0] = new Point(1.0);
+		pointMap[0][1] = null; pointMap[1][1] = new Point(1.0); pointMap[2][1] = null; pointMap[3][1] = null; pointMap[4][1] = new Point(1.0); pointMap[5][1] = null; pointMap[6][1] = new Point(1.0);
+		pointMap[0][2] = new Point(1.0); pointMap[1][2] = new Point(1.0); pointMap[2][2] = new Point(1.0); pointMap[3][2] = null; pointMap[4][2] = new Point(1.0); pointMap[5][2] = null; pointMap[6][2] = new Point(1.0);
+		pointMap[0][3] = new Point(1.0); pointMap[1][3] = null; pointMap[2][3] = new Point(1.0); pointMap[3][3] = null; pointMap[4][3] = new Point(1.0); pointMap[5][3] = null; pointMap[6][3] = new Point(1.0);
+		pointMap[0][4] = new Point(1.0); pointMap[1][4] = null; pointMap[2][4] = new Point(1.0); pointMap[3][4] = new Point(1.0); pointMap[4][4] = new Point(1.0); pointMap[5][4] = null; pointMap[6][4] = new Point(1.0);
+		pointMap[0][5] = new Point(1.0); pointMap[1][5] = new Point(1.0); pointMap[2][5] = new Point(1.0); pointMap[3][5] = null; pointMap[4][5] = null; pointMap[5][5] = new Point(1.0); pointMap[6][5] = new Point(1.0);
+		pointMap[0][6] = null; pointMap[1][6] = null; pointMap[2][6] = null; pointMap[3][6] = null; pointMap[4][6] = null; pointMap[5][6] = null; pointMap[6][6] = new Point(1.0);
 	}
 
 	/**
